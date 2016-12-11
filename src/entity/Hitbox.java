f908b632852bf86e83ffa5dd27a6c9a8677f84bb
Bident <ianonthe@gmail.com>
2016-12-10 22:55:12 -0500

    Hitboxes dun diddley work need fix

--- src/entity/Hitbox.java
+++ src/entity/Hitbox.java
@@ -4,12 +4,12 @@ import java.awt.geom.Rectangle2D;
 import java.util.List;
 
 public class Hitbox {
-	private List<Rectangle2D.Double> boxes;
-
+	List<Rectangle2D.Double> boxes;
+	
 	public Hitbox(List<Rectangle2D.Double> boxes){
 		this.boxes = boxes;
 	}
-
+	
 	public List<Rectangle2D.Double> getBoxes(){
 		return boxes;
 	}
