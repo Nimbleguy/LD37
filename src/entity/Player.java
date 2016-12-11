38e165e004803fce8e2c92b0478807dc1c13148c
Nimbleguy <nomblest@gmail.com>
2016-12-10 22:51:31 -0500

    "what did you d'un diddily edit" the world may never know ... ... ... Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit. On branch master Your branch is up-to-date with 'origin/master'. Changes to be committed: 	new file:   src/entity/Player.java

---
+++ src/entity/Player.java
@@ -0,0 +1,16 @@
+package entity;
+
+import java.awt.Image;
+import java.awt.geom.Rectangle2D;
+import java.util.ArrayList;
+import java.util.Arrays;
+import javax.swing.ImageIcon;
+
+import core.util.Convert;
+
+public class Player extends Entity{
+	public Player(double x, double y){
+		super(new ArrayList<Image>(Arrays.asList(new Image[] {new ImageIcon("assets/sprites/player.png").getImage()})), new Hitbox(new ArrayList<Rectangle2D.Double>(Arrays.asList(new Rectangle2D.Double(x, y, Convert.perWidth(new ImageIcon("assets/sprites/player.png").getImage().getWidth(null)), Convert.perHeight(new ImageIcon("assets/sprites/player.png").getImage().getHeight(null)))))), x, y);
+		this.setSpriteIndex(0);
+	}
+}
