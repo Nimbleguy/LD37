7dd96e1c87d905163eae4eecba293a361d4c19f1
Bident <ianonthe@gmail.com>
2016-12-10 20:33:48 -0500

    added collision events

--- src/events/CollisionEvent.java
+++ src/events/CollisionEvent.java
@@ -7,23 +7,52 @@ import java.lang.annotation.Target;
 import java.lang.reflect.Method;
 
 import core.Game;
+import entity.Entity;
 
 public class CollisionEvent implements Event{
+
+	private boolean cancelled;
+	private Entity e1;
+	private Entity e2;
+	public CollisionEvent(Entity agressor, Entity defender){
+		e1 = agressor;
+		e2 = defender;
+		cancelled = false;
+	}
+	
+	
+	public void setCancelled(boolean cancel){
+		cancelled = cancel;
+	}
+	
+	public boolean isCancelled(){
+		return cancelled;
+	}
+	
+	public Entity getAgressor(){
+		return e1;
+	}
+	
+	public Entity getDefender(){
+		return e2;
+	}
 	
 	@Retention(value=RetentionPolicy.RUNTIME)
 	@Target(value=ElementType.METHOD)
-	@interface Listener{
-		
-	}
-	
+	@interface Listen{}
+
 	@Override
 	public boolean trigger(){
-		
-		for (Listener l : Game.getInstance().getListeners()){
-			Method m = l.getClass().getMethod("listen", Event.class);
-			if (){
-				
+		try{
+			for (Listener l : Game.getInstance().getListeners()){
+				Method m = l.getClass().getMethod("listen", Event.class);
+				if (m.isAnnotationPresent(Listen.class)){
+					l.listen(this);
+				}
 			}
+		}catch (NoSuchMethodException | SecurityException e){
+			e.printStackTrace();
 		}
+		return !cancelled;
 	}
 }
